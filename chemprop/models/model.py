783ca72d4306b90087bedab2216957a21806c75e
tingweidaniel <42811570+tingweidaniel@users.noreply.github.com>
2019-10-08 22:13:48 +0800

    Fix

--- chemprop/models/model.py
+++ chemprop/models/model.py
@@ -88,7 +88,6 @@ class MoleculeModel(nn.Module):
         :return: The output of the MoleculeModel.
         """
         output = self.ffn(self.encoder(*input))
-        print(output)
         # Don't apply sigmoid during training b/c using BCEWithLogitsLoss
         if self.classification and not self.training:
             output = self.sigmoid(output)
@@ -126,20 +125,7 @@ class LambdaLayer(nn.Module):
         self.lambda_function = lambda_function
     def forward(self, x):
         return self.lambda_function(x)
-
-'''
-# This cannot work.
-class TimeDistributed_wrapper(nn.Module):
-    def __init__(self, working_layer):
-        super(TimeDistributed_wrapper, self).__init__()
-        self.working_layer = working_layer
-    def forward(self, mol_vector):
-        final_mole_vector = []
-        for atom_vector in mol_vector:
-            atom_vector = self.working_layer(atom_vector)
-            final_mole_vector.append(atom_vector)
-        return final_mole_vector
-'''
+    
 
 class TimeDistributed_wrapper(nn.Module):
     def __init__(self, module, batch_first=False):
